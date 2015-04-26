2a0fed9f74b4c2c2cdff460e2cdbd8e433501626
Stanislav Vojíř <stanislav.vojir@gmail.com>
2015-04-26 21:28:43 +0200

    Přidána PHP třída pro integraci s EasyMinerCenter

---
+++ Integration.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace EasyMiner\Help;
+
+class Integration {
+  /**
+   * @var $javascriptFiles - this array contains list of javascript files for inclusion into page
+   */
+  public static $javascriptFiles = [
+    'js/helpFunctions.js',
+    'js/help.js'
+  ];
+
+  /**
+   * @var $cssFiles - this array contains list of CSS files for inclusion into page
+   */
+  public static $cssFiles = [
+    'css/help.css'
+  ];
+
+}
