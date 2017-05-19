196504d5857d1219c38678a9dfdcc4e5de76e8ef
Stanislav Vojíř <stanislav.vojir@gmail.com>
2017-05-19 22:20:50 +0200

    changed license to Apache 2.0

--- Integration.php
+++ Integration.php
@@ -2,6 +2,12 @@
 
 namespace EasyMiner\Help;
 
+/**
+ * Class Integration - class for integration of EasyMiner-Help to EasyMiner-EasyMinerCenter
+ * @package EasyMiner\Help
+ * @author Stanislav Vojíř
+ * @license http://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
+ */
 class Integration {
   /**
    * @var $javascriptFiles - this array contains list of javascript files for inclusion into page
--- js/help.js
+++ js/help.js
@@ -1,5 +1,7 @@
 /**
  * Definition of the EasyMiner Help class
+ * @author Petr Hanišák
+ * @license http://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
  * @param params : object (optional)
  * @constructor
  */
